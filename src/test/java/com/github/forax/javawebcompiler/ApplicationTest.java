25edb964db70755c8b8355198b63fa85e5f90dbe
ihssanebennani7 <ihssanebennani7@gmail.com>
2026-04-28 15:57:56 +0200

    last tests

--- src/test/java/com/github/forax/javawebcompiler/ApplicationTest.java
+++ src/test/java/com/github/forax/javawebcompiler/ApplicationTest.java
@@ -85,4 +85,47 @@ public final class ApplicationTest {
       var output = Runner.runFromMemory("Main", loader);
       assertEquals("", output);
     }
+
+  @Test
+  public void compileWithDifferentClassName() {
+    var code = """
+      public class Test {
+        public static void main(String[] args) {
+        }
+      }
+      """;
+    var loader = new MemoryClassLoader();
+    var diagnostics = Compiler.compileInMemory("Test", code,loader);
+
+    assertTrue(diagnostics.isEmpty());
+  }
+
+  @Test
+  public void compileWithWrongClassNameShouldFail() {
+    var code = """
+      public class Test {
+      }
+      """;
+
+    var loader = new MemoryClassLoader();
+    var diagnostics = Compiler.compileInMemory("Main", code,loader);
+
+    assertFalse(diagnostics.isEmpty());
+  }
+
+  @Test
+  public void compileClassWithoutMainShouldStillCompile() {
+    var code = """
+        public class Test {
+            int x = 10;
+        }
+        """;
+
+    var loader = new MemoryClassLoader();
+    var diagnostics = Compiler.compileInMemory("Test", code,loader);
+
+    assertTrue(diagnostics.isEmpty());
+  }
+
+
 }
